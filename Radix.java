3e033dc0beb6a385018b9936ed2681628ccca185
Qiong <qhuang00@stuy.edu>
2019-04-01 13:18:50 -0400

    working on problem with extend and radix

--- Radix.java
+++ Radix.java
@@ -1,8 +1,8 @@
 public class Radix{
   public static void radixsort(int[] data){
     MyLinkedList[] buckets = new MyLinkedList[10];
-    for (MyLinkedList<Integer> buc: buckets) {
-      buc = new MyLinkedList<Integer>();
+    for (int i = 0; i < buckets.length; i++) {
+      buckets[i] = new MyLinkedList<Integer>();
     }
     boolean completed = false;
     int place = 1;
@@ -10,6 +10,7 @@ public class Radix{
       completed = true;
       for(int num: data){
         int index = (num % (10 * place)) / place;
+        System.out.println("INDEX" + index);
         if(num < 0){
           buckets[index].addFront(num);
         } else{
@@ -22,8 +23,15 @@ public class Radix{
         for (MyLinkedList<Integer> buc: buckets ) {
           newData.extend(buc);
         }
-        
+        for (int x = 0;x < data.length ;x++ ) {
+          data[x] = newData.removeFront();
+          }
       }
     }
+  }
+  public static void main(String[] args) {
+    int[] a= {20,23,45,12,56,87};
+    radixsort(a);
+    System.out.println(a);
   }
 }
