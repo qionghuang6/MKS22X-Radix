c8e0819289ac7e4dd6970da2b4efdea8421fddc9
Qiong <qhuang00@stuy.edu>
2019-04-01 10:07:33 -0400

    began to write radix sort

---
+++ Radix.java
@@ -0,0 +1,15 @@
+public class Radix{
+  public static void radixsort(int[] data){
+    MyLinkedList[] buckets = new MyLinkedList[10];
+    for (MyLinkedList<Integer> buc: buckets) {
+      buc = new MyLinkedList<Integer>();
+    }
+    boolean completed = false;
+    int place = 1;
+    while(!completed){
+      for(int num: data){
+
+      }
+    }
+  }
+}
