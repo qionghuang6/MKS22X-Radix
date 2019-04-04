3a8b1e860be789a0f0b4946654e1174b51dbeca6
Qiong <qhuang00@stuy.edu>
2019-04-03 20:01:30 -0400

    tried to optmize some changed back

--- Radix.java
+++ Radix.java
@@ -42,20 +42,38 @@ public class Radix{
       data[x] = newData.removeFront();
     }
   }
-  public static void main(String[] args) {
-    /*
-    int[] a= new int[25];
-    for (int x = 0;x < 25 ;x++ ) {
-      a[x] = (int) (Math.random() * 1000);
-    } */
-    //System.out.println(;
-    int[] a = {213,-1000000,-100,-99,-992192};
-    int [] b = Arrays.copyOf(a,a.length);
-    Arrays.sort(b);
-    System.out.println(Arrays.toString(a));
-    radixsort(a);
-    System.out.println(Arrays.toString(a));
-    System.out.println(Arrays.toString(b));
-    System.out.println(Arrays.equals(a,b));
+  public static void main(String[]args){
+    System.out.println("Size\t\tMax Value\tradix/builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          Radix.radixsort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
+        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+      }
+      System.out.println();
     }
+  }
 }
